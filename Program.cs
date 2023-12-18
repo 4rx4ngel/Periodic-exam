e2ecbe93a99b9c29bb5838d6b2d2fd73f9d73bf2
Mikhail_Biziukov <HostProcessor@gmail.com>
2023-12-18 14:39:58 +0700

    finished Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,69 @@
-﻿
+﻿string[] GetArrayStringConsole (string inConsoleSymbol)
+{
+    string[] arraySymbol = new string[inConsoleSymbol.Length];
+    arraySymbol = inConsoleSymbol.Split(' ');
+    return arraySymbol;
+}
+
+string[] GetArrayThreeSymbol (string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        string symbol = array[i];
+        if (symbol.Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] arrayThreeSymbol = new string[count];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        string symbol = array[i];
+        if (symbol.Length <= 3)
+        {
+            arrayThreeSymbol[j] = symbol;
+            j++;
+        }
+    }
+    return arrayThreeSymbol;
+}
+
+void Print (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("");
+
+}
+
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.InputEncoding = System.Text.Encoding.Unicode;
+
+Console.Clear();
+
+Console.Write("Введите набор символов через пробел: ");
+string inStringSymbol = Console.ReadLine();
+string[] arraySymbol = GetArrayStringConsole(inStringSymbol);
+
+string[] arrayThreeSymbol = GetArrayThreeSymbol(arraySymbol);
+
+Console.Clear();
+
+Console.Write("Введенные символы: ");
+Print(arraySymbol);
+
+Console.WriteLine();
+
+Console.Write("Введенные символы длиной меньше либо равны 3: ");
+Print(arrayThreeSymbol);
+
+Console.WriteLine();
+
+
